7b4ae2e68926f561257d169f40e5cedec204d5a4
Alessandro <alessandrodini97@gmail.com>
2026-02-18 12:25:37 -0300

    Implementa función de menú principal

---
+++ main.py
@@ -0,0 +1,6 @@
+def menu(): 
+    print("1. Agregar gastos")
+    print("2. Ver gastos hasta la fecha")
+    print("3. Ver gastos por categoría")
+    print("4. Ver gastos del mes y presupuesto")
+    print("5. Salir")
