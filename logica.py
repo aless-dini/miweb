13a3054bd849b385fa64865861650c37d51a2c29
Alessandro <alessandrodini97@gmail.com>
2026-02-19 09:50:39 -0300

    Renombra gastos.py a logica.py

---
+++ logica.py
@@ -0,0 +1,18 @@
+from storage import guardar_info_gastos
+
+def agregar_gasto(gastos):
+    precio = float(input("Escribe el precio: "))
+    categoria = input("Escribe la categoría: ")
+    descripcion = input("Escribe una descripción: ")
+    fecha = input("Escribe la fehca: ")
+    
+    gasto = {
+        "precio": precio,
+        "categoria": categoria,
+        "descripcion": descripcion,
+        "fecha" : fecha
+    }
+    
+    gastos.apppend(gasto)
+    guardar_info_gastos(gastos)
+    print("Gasto agregado correctamente")
